5cb18430b55140451b29c183c8a11f101feea487
kylebascomb <kylebascomb@gmail.com>
2020-10-19 04:04:07 -0700

    Fixed resizing issues

--- hardware/src/controller/Controller.java
+++ hardware/src/controller/Controller.java
@@ -5,6 +5,7 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import view.View;
 
 public class Controller {
@@ -15,14 +16,22 @@ public class Controller {
 
     public void passControl(Controller controller, ActionEvent e){
         View newView = controller.getView();
+        Window window = ((Node)e.getSource()).getScene().getWindow();
+        
+        double height = window.getHeight();
+        double width = window.getWidth();
+        double x = window.getX();
+        double y = window.getY();
+
         Stage primaryStage = (Stage)((Node)e.getSource()).getScene().getWindow();
         primaryStage.setScene(newView.getScene());
 
-        Screen screen = Screen.getPrimary();
-        Rectangle2D bounds = screen.getVisualBounds();
-        primaryStage.setWidth(bounds.getWidth());
-        primaryStage.setHeight(bounds.getHeight());
+        primaryStage.setWidth(width);
+        primaryStage.setHeight(height);
+        primaryStage.setX(x);
+        primaryStage.setY(y);
         primaryStage.setMaximized(true);
+
     }
 
     /**
