316d344dd5388d12a0ced56fa27b568dc35d7b7d
kylebascomb <kylebascomb@gmail.com>
2020-10-19 19:24:57 -0700

    Added absolute minimum model implemenation in order to get serialization working. Now any products added to the inventory should save

--- hardware/src/controller/StartupController.java
+++ hardware/src/controller/StartupController.java
@@ -1,10 +1,27 @@
 package controller;
 
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import model.Inventory;
 import view.StartupView;
+
+import java.io.*;
 
 public class StartupController extends Controller{
 
     public StartupController() {
+        this.view = new StartupView();
+        startupView = (StartupView)view;
+
+        inventory = new Inventory();
+        getAllData();
+
+        initControllers();
+    }
+
+    public StartupController(Inventory inventory){
+        this.inventory = inventory;
+
         this.view = new StartupView();
         startupView = (StartupView)view;
 
@@ -13,12 +30,77 @@ public class StartupController extends Controller{
 
     public void initControllers(){
         startupView.getAdminButton().setOnAction(e ->{
-            passControl(new AdminController(), e);
+            passControl(new AdminController(inventory), e);
         });
 
         startupView.getCustomerButton().setOnAction(e ->{
             passControl(new CustomerController(), e);
         });
+
+        startupView.getSaveButton().setOnAction(e ->{
+            saveAllData();
+            Stage primaryStage = (Stage)((Node)e.getSource()).getScene().getWindow();
+            primaryStage.close();
+        });
+    }
+
+
+    public void saveAllData(){
+        String filename = this.FILE_PATH;
+
+        // Serialization
+        try
+        {
+            //Saving of object in a file
+            FileOutputStream file = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(file);
+
+            // Method for serialization of object
+            out.writeObject(inventory);
+
+            out.close();
+            file.close();
+
+            System.out.println("Object has been serialized");
+
+        }
+        catch(IOException ex)
+        {
+            System.out.println("OException is caught");
+            System.out.println(ex.getMessage());
+            System.out.println(ex.getCause());
+        }
+    }
+
+    public boolean getAllData(){
+        String filename = FILE_PATH;
+        try
+        {
+            // Reading the object from a file
+            FileInputStream file = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(file);
+
+            // Method for deserialization of object
+            inventory = (Inventory)in.readObject();
+
+            in.close();
+            file.close();
+
+            System.out.println("Object has been deserialized ");
+        }
+
+        catch(IOException ex)
+        {
+            System.out.println("IException is caught");
+            return false;
+        }
+
+        catch(ClassNotFoundException ex)
+        {
+            System.out.println("ClassNotFoundException is caught");
+            return false;
+        }
+        return true;
     }
 
 
@@ -41,4 +123,7 @@ public class StartupController extends Controller{
     }
 
     private StartupView startupView;
+    private Inventory inventory;
+
+    private String FILE_PATH = "./hardware/resources/inventory.dat";
 }
--- hardware/src/model/Inventory.java
+++ hardware/src/model/Inventory.java
@@ -1,9 +1,33 @@
 package model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Inventory {
-	
+public class Inventory implements Serializable {
+
+	public Inventory(ArrayList<Product> products) {
+		this.products = products;
+	}
+
+	public Inventory(){
+		products = new ArrayList<>();
+	}
+
+
+	public int getSize(){
+		return products.size();
+	}
+
+
+
+	public void addProduct(Product product){
+		products.add(product);
+	}
+
+	public void removeProduce(Product product){
+		products.remove(product);
+	}
+
 	private ArrayList<Product> products;
 
 }
