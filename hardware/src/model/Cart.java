ea3c749e38571a8b86ad6f1a3a795c68854a257b
Dylan Nguyen <dylan.c.nguyen@sjsu.edu>
2020-10-19 20:01:10 -0700

    Added cart and ways ot change quantity of certain items

---
+++ hardware/src/model/Cart.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Cart extends Inventory {
+	
+	public Cart(ArrayList<Product> products) {
+		super(products);
+	}
+	
+	public Cart() {
+		super();
+	}
+
+}
