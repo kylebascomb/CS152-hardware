fb18e0db02692b24b5026b01f22e99a07f4cb7c3
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2020-10-20 21:19:08 -0700

    Update View.java

--- hardware/src/view/View.java
+++ hardware/src/view/View.java
@@ -17,7 +17,25 @@ public class View {
     public void layoutComponents(){
 
     }
+    
+    /**
+     * Getter for stage
+     *
+     * @return stage
+     */
+    public Stage getStage() {
+        return stage;
+    }
 
+    /**
+     * Setter for stage
+     *
+     * @param stage - stage
+     */
+    public void setStage(Stage scene) {
+        this.stage = stage;
+    }
+    
     /**
      * Getter for scene
      *
@@ -35,6 +53,7 @@ public class View {
     public void setScene(Scene scene) {
         this.scene = scene;
     }
-
+    
+    protected Stage stage;
     protected Scene scene;
 }
