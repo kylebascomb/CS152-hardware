2fe3f563b2ffb31c9355ed6fb431b8537da25fee
Kyle <kylebascomb@gmail.com>
2020-11-02 15:59:00 -0800

    Added a MenuBar to AdminView to switch between viewing the inventory and the report

---
+++ hardware/src/view/components/ReportBox.java
@@ -0,0 +1,41 @@
+package view.components;
+
+import javafx.scene.layout.HBox;
+
+public class ReportBox extends HBox {
+
+
+    public ReportBox() {
+        initComponents();
+        layoutComponents();
+    }
+
+    private void initComponents() {
+        reportTable = new ReportTableView();
+        cartTable = new CartTableView();
+    }
+
+    private void layoutComponents(){
+        getChildren().addAll(reportTable, cartTable);
+    }
+
+
+    public ReportTableView getReportTable() {
+        return reportTable;
+    }
+
+    public void setReportTable(ReportTableView reportTable) {
+        this.reportTable = reportTable;
+    }
+
+    public CartTableView getCartTable() {
+        return cartTable;
+    }
+
+    public void setCartTable(CartTableView cartTable) {
+        this.cartTable = cartTable;
+    }
+
+    private ReportTableView reportTable;
+    private CartTableView cartTable;
+}
