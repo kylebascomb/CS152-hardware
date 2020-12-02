d0c3bb06d60d497c6f2e6020dd74c0034236ff9c
Dylan Nguyen <62354680+MineYoFaceGit@users.noreply.github.com>
2020-12-01 20:44:42 -0800

    Update Controller.java
    comments

--- hardware/src/controller/Controller.java
+++ hardware/src/controller/Controller.java
@@ -6,12 +6,22 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 import view.View;
 
+/**
+ * 
+ * Superclass of all controllers
+ *
+ */
 public class Controller {
 
     public Controller(){
 
     }
 
+    /**
+     * Passes control to another controller
+     * @param controller controller to be given control
+     * @param e event that triggered passing
+     */
     public void passControl(Controller controller, ActionEvent e){
         View newView = controller.getView();
         Window window = ((Node)e.getSource()).getScene().getWindow();
@@ -21,9 +31,11 @@ public class Controller {
         double x = window.getX();
         double y = window.getY();
 
+        //change to new scene of controlller
         Stage primaryStage = (Stage)((Node)e.getSource()).getScene().getWindow();
         primaryStage.setScene(newView.getScene());
-
+        
+        //keeps window size constant
         primaryStage.setWidth(width);
         primaryStage.setHeight(height);
         primaryStage.setX(x);
