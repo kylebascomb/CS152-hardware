d66ebf7f49b02304fd9fcbadb8892eedec04499c
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2020-12-01 23:49:24 -0800

    Create ReportTests.java

---
+++ hardware/src/JUnitTests/ReportTests.java
@@ -0,0 +1,38 @@
+package JUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Report;
+
+public class ReportTests {
+
+	public void setup() {
+		
+	}
+	
+	@Before
+	public void init() {
+		
+	}
+	
+	@Test 
+	public void getSizeTest() {
+		
+	}
+	
+	@Test
+	public void addReceiptTest() {
+		
+	}
+	
+	@After
+	public void destroy() {
+		
+	}
+	
+	private Report report = new Report();
+}
