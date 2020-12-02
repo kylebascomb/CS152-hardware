b89dfca04bf9d212f7596d8fdd5f6e2ac26d1a66
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2020-12-01 17:36:42 -0800

    Created JUnitTest package.

---
+++ hardware/src/JUnitTests/AdminControllerTests.java
@@ -0,0 +1,14 @@
+package JUnitTests;
+
+import static org.junit.Assert.*;
+import controller.*;
+import org.junit.Test;
+
+public class AdminControllerTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ hardware/src/JUnitTests/CustomerControllerTests.java
@@ -0,0 +1,15 @@
+package JUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import controller.*;
+
+public class CustomerControllerTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
