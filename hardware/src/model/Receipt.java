87f1aa4de8b0d345fcb3d02ebb4b7573e09dd4ee
Dylan Nguyen <62354680+MineYoFaceGit@users.noreply.github.com>
2020-12-01 21:12:11 -0800

    Update Receipt.java
    comments

--- hardware/src/model/Receipt.java
+++ hardware/src/model/Receipt.java
@@ -1,77 +1,65 @@
 package model;
 
+import javafx.scene.control.MenuBar;
+
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-public class Receipt implements Serializable{
-	
-	/**
-	 * Constructor 
-	 * @param cart cart that is purchased for given receipt
-	 */
-	public Receipt(Cart cart) {
-		datetime = LocalDateTime.now();
-		timeStamp = computeTimestamp();
-		this.cart = cart;
-		total=0;
-		for(Product p : cart.getProducts()) {
-			total += p.getPrice() * (float)p.getQuantity();
-		}
-	}
-	
-	/**
-	 * Returns formatted timestamp string
-	 * @return formatted timestamp string
-	 */
-	public String computeTimestamp() {
-		return datetime.getHour() + ":" + datetime.getMinute() + " "
-				+ datetime.getMonth() + " " + datetime.getDayOfMonth()
-				+ ", " + datetime.getYear();
-	}
-	
-	/**
-	 * returns cart
-	 * @return cart
-	 */
-	public Cart getCart() {
-		return cart;
-	}
-	
-	/**
-	 * returns total price
-	 * @return total price
-	 */
-	public float getTotal() {
-		return total;
-	}
-	
-	/**
-	 * returns datetime
-	 * @return datetime
-	 */
-	public LocalDateTime getDateTime() {
-		return datetime;
-	}
-	
-	/**
-	 * Returns timestamp
-	 * @return timestamp
-	 */
-	public String getTimeStamp() {
-		return timeStamp;
-	}
-	
-	/**
-	 * Sets timestamp
-	 * @param timeStamp timestamp to be set
-	 */
-	public void setTimeStamp(String timeStamp) {
-		this.timeStamp = timeStamp;
-	}
+/**
+ * 
+ * Collection of receipts for past purchases
+ *
+ */
+public class Report implements Serializable {
 
-	private Cart cart;
-	private float total;
-	private LocalDateTime datetime;
-	private String timeStamp;
+	/**
+	 * Constructor
+	 * @param report list of reports
+	 */
+    public Report(ArrayList<Receipt> report) {
+        this.report = report;
+    }
+
+    /**
+     * Constructor for fresh report 
+     */
+    public Report() {
+        report = new ArrayList<>();
+    }
+
+    /**
+     * return size of list
+     * @return size of list
+     */
+    public int getSize() {
+        return report.size();
+    }
+
+    /**
+     * Add receipt to report
+     * @param receipt receipt to be added
+     */
+    public void addReceipt(Receipt receipt) {
+        report.add(receipt);
+    }
+
+    /**
+     * returns report
+     * @return report
+     */
+    public ArrayList<Receipt> getReport() {
+        return report;
+    }
+
+    /**
+     * Sets report tog iven list
+     * @param report new report list
+     */
+    public void setReport(ArrayList<Receipt> report) {
+        this.report = report;
+    }
+
+    private ArrayList<Receipt> report;
+
 
 }
