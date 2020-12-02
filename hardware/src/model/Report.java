5ab199a8c72bc2d3b161a44210ca0af3384b7dbb
Dylan Nguyen <62354680+MineYoFaceGit@users.noreply.github.com>
2020-12-01 21:15:01 -0800

    Update Report.java

--- hardware/src/model/Report.java
+++ hardware/src/model/Report.java
@@ -5,37 +5,56 @@ import javafx.scene.control.MenuBar;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * 
+ * Collection of receipts for past purchases
+ *
+ */
 public class Report implements Serializable {
 
+	/**
+	 * Constructor
+	 * @param report list of reports
+	 */
     public Report(ArrayList<Receipt> report) {
         this.report = report;
     }
 
+    /**
+     * Constructor for fresh report 
+     */
     public Report() {
         report = new ArrayList<>();
     }
 
+    /**
+     * return size of list
+     * @return size of list
+     */
     public int getSize() {
         return report.size();
     }
 
     /**
-     * public Receipt getReceipt() {
-     * //return receipt;
-     * }
+     * Add receipt to report
+     * @param receipt receipt to be added
      */
-
-
     public void addReceipt(Receipt receipt) {
-        //TODO does this need to be cloned?
         report.add(receipt);
     }
 
-
+    /**
+     * returns report
+     * @return report
+     */
     public ArrayList<Receipt> getReport() {
         return report;
     }
 
+    /**
+     * Sets report tog iven list
+     * @param report new report list
+     */
     public void setReport(ArrayList<Receipt> report) {
         this.report = report;
     }
