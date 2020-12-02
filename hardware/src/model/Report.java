b7653d6bc317894771581a4609259b517e612436
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2020-12-02 01:05:49 -0800

    Removed unused imports.

--- hardware/src/model/Report.java
+++ hardware/src/model/Report.java
@@ -1,7 +1,5 @@
 package model;
 
-import javafx.scene.control.MenuBar;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 
--- hardware/src/view/AdminView.java
+++ hardware/src/view/AdminView.java
@@ -1,13 +1,11 @@
 package view;
 
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import view.components.*;
