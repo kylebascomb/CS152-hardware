4f53d2d1c7b5cc08a20e3ab48e59611f376c9018
Dylan Nguyen <62354680+MineYoFaceGit@users.noreply.github.com>
2020-12-01 21:03:29 -0800

    Update Inventory.java
    comments

--- hardware/src/model/Inventory.java
+++ hardware/src/model/Inventory.java
@@ -3,25 +3,43 @@ package model;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * 
+ * Inventory of store. Stores products
+ *
+ */
 public class Inventory implements Serializable {
 
+	/**
+	 * Constructor with set of products
+	 * @param products initial set of products
+	 */
 	public Inventory(ArrayList<Product> products) {
 		this.products = products;
 	}
-
+	
+	/**
+	 * base constructor for fresh inventory
+	 */
 	public Inventory(){
 		products = new ArrayList<>();
 	}
-
-
+	
+	/**
+	 * return size of inventory
+	 * @return size of inventory
+	 */
 	public int getSize(){
 		return products.size();
 	}
-
-
-
+	
+	/**
+	 * Returns product with given id
+	 * @param productID id of desired product
+	 * @return product with id
+	 */
 	public Product getProduct(int productID){
-		//TODO optimize this?
+		//searches through array list for product with given id
 		for(Product product: products){
 			if(product.getProductId() == productID){
 				return product;
@@ -30,25 +48,36 @@ public class Inventory implements Serializable {
 		return null;
 	}
 
-
+	/**
+	 * Adds product straight to inventory
+	 * @param product product to be added
+	 */
 	public void addProduct(Product product){
 		products.add(product.clone());
 	}
 	
-	
+	/**
+	 * Adds certain amount of given product. If product exists
+	 * it adds the quantity and adds new product if none else exists.
+	 * @param product product to be added
+	 * @param amount amount to be added
+	 */
 	public void addProduct(Product product, int amount) {
 		int index = -1;
 		int id = product.getProductId();
+		//searches through ivnentory to find product with same id
 		for(int i = 0; i < products.size(); i++) {
 			if(products.get(i).getProductId() == id) {
 				index = i;
 				i = products.size();
 			}
 		}
+		//updates quantity if product found
 		if(index > -1) {
 			Product p = products.get(index);
 			p.setQuantity(p.getQuantity() + amount);
 		}
+		//adds new product if none found
 		else {
 			Product temp = product.clone();
 			temp.setQuantity(amount);
@@ -56,23 +85,34 @@ public class Inventory implements Serializable {
 		}
 	}
 
-	
+	/**
+	 * removes product 
+	 * @param product product to be removed
+	 */
 	public void removeProduct(Product product){
 		products.remove(product);
 	}
 	
-	
+	/**
+	 * Removes a certain amount of product fro inventory
+	 * @param product product to be removed
+	 * @param amount amount to be removed
+	 * @return true if there is enough of product to remove or false otherwise
+	 */
 	public boolean removeProduct(Product product, int amount) {
 		int index = -1;
 		int id = product.getProductId();
+		//tries to find product
 		for(int i = 0; i < products.size(); i++) {
 			if(products.get(i).getProductId() == id) {
 				index = i;
 				i = products.size();
 			}
 		}
+		//if item found tries to remove quantity
 		if(index > -1) {
 			Product p = products.get(index);
+			//removes if enough quantity
 			if(p.getQuantity() >= amount) {
 				p.setQuantity(p.getQuantity() - amount);
 				return true;
