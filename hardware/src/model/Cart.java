75de946fd9510d5b878cccc569d6ffe7a743048e
Dylan Nguyen <62354680+MineYoFaceGit@users.noreply.github.com>
2020-12-01 21:30:57 -0800

    Update Cart.java

--- hardware/src/model/Cart.java
+++ hardware/src/model/Cart.java
@@ -1,152 +1,40 @@
 package model;
 
-import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * 
- * Product within the store. Stores values related to product.
+ * Holds a collection of products to be purchased
  *
  */
-public class Product implements Serializable {
-
-
+public class Cart extends Inventory {
+	
 	/**
-	 * Constructor
-	 * @param name
-	 * @param price
-	 * @param quantity
-	 * @param description
-	 * @param type
-	 * @param productId
+	 * Constructor with initial set of products
+	 * @param products initial set of products
 	 */
-    public Product(String name, float price, int quantity, String description, String type, int productId) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.description = description;
-        this.type = type;
-        this.productId = productId;
-    }
+	public Cart(ArrayList<Product> products) {
+		super(products);
+	}
+	
+	/**
+	 * Base Constructor
+	 */
+	public Cart() {
+		super();
+	}
+	
+	/**
+	 * Returns total price of all items in the cart
+	 * @return total price of items in cart
+	 */
+	public float getTotal() {
+		float total = 0;
+		//goes through every product and  adds product of quantity and price to total
+		for(Product p : super.getProducts()) {
+			total += (float)p.getQuantity() * p.getPrice();
+		}
+		return total;
+	}
 
-    /**
-     * Getter for name
-     *
-     * @return name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Setter for name
-     *
-     * @param name - name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Getter for price
-     *
-     * @return price
-     */
-    public float getPrice() {
-        return price;
-    }
-
-    /**
-     * Setter for price
-     *
-     * @param price - price
-     */
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    /**
-     * Getter for quantity
-     *
-     * @return quantity
-     */
-    public int getQuantity() {
-        return quantity;
-    }
-
-    /**
-     * Setter for quantity
-     *
-     * @param quantity - quantity
-     */
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    /**
-     * Getter for description
-     *
-     * @return description
-     */
-    public String getDescription() {
-        return description;
-    }
-
-    /**
-     * Setter for description
-     *
-     * @param description - description
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    /**
-     * Getter for productId
-     *
-     * @return productId
-     */
-    public int getProductId() {
-        return productId;
-    }
-
-    /**
-     * Setter for productId
-     *
-     * @param productId - productId
-     */
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    /**
-     * Getter for type
-     *
-     * @return type
-     */
-    public String getType() {
-        return type;
-    }
-
-    /**
-     * Setter for type
-     *
-     * @param type - type
-     */
-    public void setType(String type) {
-        this.type = type;
-    }
-    
-    /**
-     * returns clone of Product
-     */
-    public Product clone() {
-    	return new Product(name, price, quantity, description, type, productId);
-    }
-
-    private String name;
-    private float price;
-    private int quantity;
-    private String description;
-    private int productId;
-    private String type;
 }
