cd42c963e9b6304b516e546e93e6c9c54c147fab
Dylan Nguyen <62354680+MineYoFaceGit@users.noreply.github.com>
2020-12-01 21:57:21 -0800

    Update CartTableView.java

--- hardware/src/view/components/CartTableView.java
+++ hardware/src/view/components/CartTableView.java
@@ -3,13 +3,25 @@ package view.components;
 import model.Inventory;
 import view.components.ProductTableView;
 
+/**
+ * 
+ * Extension of product table except for cart
+ *
+ */
 public class CartTableView extends ProductTableView {
 	
+	/**
+	 * Constructor
+	 * @param inventory inventory to be passsed
+	 */
 	public CartTableView(Inventory inventory) {
 		super(inventory);
 		super.titleColumn.setText("Cart");
 	}
 	
+	/**
+	 * Constructor
+	 */
 	public CartTableView() {
 		super();
 		super.titleColumn.setText("Cart");
