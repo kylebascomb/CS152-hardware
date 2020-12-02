e1f370424a031358297e1f429287bf3c11186646
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2020-12-01 22:44:29 -0800

    Update ReceiptTests.java

--- hardware/src/JUnitTests/ReceiptTests.java
+++ hardware/src/JUnitTests/ReceiptTests.java
@@ -2,13 +2,32 @@ package JUnitTests;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import model.Cart;
 
 public class ReceiptTests {
 
+	public void setup() {
+	}
+	
+	
+	@Before
+	public void init() {
+		
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+	}
+	
+	@After
+	public void destroy() {
+		
 	}
 
+	private Cart cart = new Cart();
 }
