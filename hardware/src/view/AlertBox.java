6c5ecce92920d97553d4137976cf3a96f9f46377
Dylan Nguyen <62354680+MineYoFaceGit@users.noreply.github.com>
2020-12-01 21:40:57 -0800

    Update AlertBox.java

--- hardware/src/view/AlertBox.java
+++ hardware/src/view/AlertBox.java
@@ -6,9 +6,19 @@ import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
 
+/**
+ * 
+ * Box for alerts that must be addressed to continue
+ *
+ */
 public class AlertBox 
 {
 
+	/**
+	 * Constructor
+	 * @param title title of box
+	 * @param message message in box
+	 */
   public AlertBox(String title, String message ){
     this.title = title;
     this.message = message;
@@ -16,6 +26,10 @@ public class AlertBox
     this.cancelButton = new Button("Cancel");
     //display();
   }
+  
+  /**
+   * layouts and then shows box
+   */
   public void display()
   {
     window = new Stage();
@@ -44,35 +58,66 @@ public class AlertBox
   }
 
 
-
+  /**
+   * returns close button
+   * @return close button
+   */
   public Button getCloseButton() {
     return closeButton;
   }
 
+  /**
+   * sets close butto
+   * @param closeButton new closebutton
+   */
   public void setCloseButton(Button closeButton) {
     this.closeButton = closeButton;
   }
 
+  /**
+   * reutrn title
+   * @return title
+   */
   public String getTitle() {
     return title;
   }
 
+  /**
+   * sets title
+   * @param title new title
+   */
   public void setTitle(String title) {
     this.title = title;
   }
 
+  /**
+   * return message
+   * @return the message
+   */
   public String getMessage() {
     return message;
   }
 
+  /**
+   * sets message
+   * @param message new message
+   */
   public void setMessage(String message) {
     this.message = message;
   }
 
+  /**
+   * returns window
+   * @return window
+   */
   public Stage getWindow() {
     return window;
   }
 
+  /**
+   * sets window
+   * @param window new window
+   */
   public void setWindow(Stage window) {
     this.window = window;
   }
