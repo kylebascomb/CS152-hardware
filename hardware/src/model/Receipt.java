2cdba07eba769600b77f0cd64f77b02ca1f43ac9
Dylan Nguyen <62354680+MineYoFaceGit@users.noreply.github.com>
2020-12-01 21:08:34 -0800

    Update Receipt.java
    comments

--- hardware/src/model/Receipt.java
+++ hardware/src/model/Receipt.java
@@ -5,6 +5,10 @@ import java.time.LocalDateTime;
 
 public class Receipt implements Serializable{
 	
+	/**
+	 * Constructor 
+	 * @param cart cart that is purchased for given receipt
+	 */
 	public Receipt(Cart cart) {
 		datetime = LocalDateTime.now();
 		timeStamp = computeTimestamp();
@@ -15,28 +19,52 @@ public class Receipt implements Serializable{
 		}
 	}
 	
+	/**
+	 * Returns formatted timestamp string
+	 * @return formatted timestamp string
+	 */
 	public String computeTimestamp() {
 		return datetime.getHour() + ":" + datetime.getMinute() + " "
 				+ datetime.getMonth() + " " + datetime.getDayOfMonth()
 				+ ", " + datetime.getYear();
 	}
 	
+	/**
+	 * returns cart
+	 * @return cart
+	 */
 	public Cart getCart() {
 		return cart;
 	}
 	
+	/**
+	 * returns total price
+	 * @return total price
+	 */
 	public float getTotal() {
 		return total;
 	}
 	
+	/**
+	 * returns datetime
+	 * @return datetime
+	 */
 	public LocalDateTime getDateTime() {
 		return datetime;
 	}
-
+	
+	/**
+	 * Returns timestamp
+	 * @return timestamp
+	 */
 	public String getTimeStamp() {
 		return timeStamp;
 	}
-
+	
+	/**
+	 * Sets timestamp
+	 * @param timeStamp timestamp to be set
+	 */
 	public void setTimeStamp(String timeStamp) {
 		this.timeStamp = timeStamp;
 	}
