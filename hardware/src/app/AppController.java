8a0442c22b74b7aa5cc98a46806490d034b1a8c3
Dylan Nguyen <62354680+MineYoFaceGit@users.noreply.github.com>
2020-12-01 20:41:30 -0800

    Update AppController.java
    Comments

--- hardware/src/app/AppController.java
+++ hardware/src/app/AppController.java
@@ -4,12 +4,19 @@ import controller.StartupController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * 
+ * Application starter
+ *
+ */
 public class AppController extends Application {
-
+    
+    //launch the app
     public static void main(String[] args){
         launch(args);
     }
-    @Override
+    
+    //starts up window to display and starts the controller that boots everything
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         StartupController startupController = new StartupController();
